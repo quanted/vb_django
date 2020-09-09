8fe97587ee6e7c92eb62025db7b84507baa485b4
Deron Smith <dsmith@Derons-MacBook-Pro.local>
2020-09-09 12:45:06 -0500

    revert start script

--- start-server.sh
+++ start-server.sh
@@ -4,9 +4,9 @@ if [ ! -d "/opt/app/vb_django/static" ]
 then
   mkdir /opt/app/vb_django/static
 fi
-python /opt/app/manage.py django-admin collectstatic --noinput
-python /opt/app/manage.py migrate --noinput /opt/app/vb_django
-python /opt/app/manage.py migrate auth --noinput /opt/app/vb_django
-python /opt/app/manage.py migrate sessions --noinput /opt/app/vb_django
-python /opt/app/manage.py runserver 0.0.0.0:8080 /opt/app/vb_django
+python /opt/app/vb_django/manage.py django-admin collectstatic --noinput
+python /opt/app/vb_django/manage.py migrate --noinput /opt/app/vb_django
+python /opt/app/vb_django/manage.py migrate auth --noinput /opt/app/vb_django
+python /opt/app/vb_django/manage.py migrate sessions --noinput /opt/app/vb_django
+python /opt/app/vb_django/manage.py runserver 0.0.0.0:8080 /opt/app/vb_django
 # exec uwsgi /etc/uwsgi/uwsgi.ini --show-config
