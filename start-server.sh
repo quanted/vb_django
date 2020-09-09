a9ef2b72ee03fd236679f51750be1ae291b19aab
Deron Smith <dsmith@Derons-MacBook-Pro.local>
2020-09-09 12:24:09 -0500

    specify absolute path to django proj directory

--- start-server.sh
+++ start-server.sh
@@ -4,9 +4,9 @@ if [ ! -d "/opt/app/vb_django/static" ]
 then
   mkdir /opt/app/vb_django/static
 fi
-django-admin collectstatic --noinput
-django-admin migrate --noinput
-django-admin migrate auth --noinput
-django-admin migrate sessions --noinput
-django-admin runserver 0.0.0.0:8080
+python /opt/app/vb_django/manage.py django-admin collectstatic --noinput
+python /opt/app/vb_django/manage.py migrate --noinput /opt/app/vb_django
+python /opt/app/vb_django/manage.py migrate auth --noinput /opt/app/vb_django
+python /opt/app/vb_django/manage.py migrate sessions --noinput /opt/app/vb_django
+python /opt/app/vb_django/manage.py runserver 0.0.0.0:8080 /opt/app/vb_django
 # exec uwsgi /etc/uwsgi/uwsgi.ini --show-config
