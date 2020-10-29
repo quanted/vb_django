677dc7d3c3e152e94d15e4ad38407a8fa9b391df
dbsmith88 <dbsmith@uga.edu>
2020-10-29 09:03:39 -0500

    added cors headers to django

--- vb_django/settings.py
+++ vb_django/settings.py
@@ -48,11 +48,13 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_extensions',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -156,3 +158,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
