0d0d533cc1fa7b11b484344ee55d61d8c5ca2e4d
Deron Smith <dsmith@Derons-MacBook-Pro.local>
2021-01-14 08:16:18 -0600

    stash

--- vb_django/settings.py
+++ vb_django/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+print("TESTING NEW CODE")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
