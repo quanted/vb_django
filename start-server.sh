da022561d6d59b13f216517bfb258ee529622990
Deron Smith <deron.brock@gmail.com>
2021-12-08 14:27:09 -0600

    updates to start script

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 python /opt/app/vb_django/manage.py collectstatic --noinput
-python /opt/app/vb_django/manage.py migrate --noinput
-python /opt/app/vb_django/manage.py auth --noinput          # used for login
-python /opt/app/vb_django/manage.py --noinput      # used for login
+python /opt/app/vb_django/manage.py migrate
+django-admin migrate auth --noinput          # used for login
+django-admin migrate sessions --noinput      # used for login
+#echo "ls /opt/app"
+#ls /opt/app
+#echo "ls /opt/app/vb_django"
+#ls /opt/app/vb_django
 exec uwsgi /etc/uwsgi/uwsgi.ini
