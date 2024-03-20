c87177e87e02e4bcd22d9d46bbf51b2e206b793e
popenc <i.nickpope@gmail.com>
2024-03-20 07:16:09 -0400

    removed noinput param from manage.py commands

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python /opt/app/manage.py collectstatic --noinput
-python /opt/app/manage.py migrate --noinput
+python /opt/app/manage.py collectstatic
+python /opt/app/manage.py migrate
 django-admin migrate auth --noinput          # used for login
 django-admin migrate sessions --noinput      # used for login
 exec uwsgi /etc/uwsgi/uwsgi.ini
